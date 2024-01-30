58a1a2509e53a10f9d9c6fc7f76374f2c8937f8f
Fuuma <masa81hiro69@gmail.com>
2024-01-30 17:58:33 +0900

    feat: add resouse user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { join } from 'path';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
